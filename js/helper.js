3ebc9447a2b903b6b81f4980d1adcbcc4a51348f
krisjhamilton <krishamilton@Kriss-iMac.local>
2015-04-08 16:14:42 +1200

    Initial Draft

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span class="myTitle">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -23,7 +23,7 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="img-responsive biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
@@ -40,7 +40,7 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" class="img-responsive">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -49,7 +49,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h4>Online Classes</h4>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -63,10 +63,10 @@ var googleMap = '<div id="map"></div>';
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
+$('button').click(function() {
+var iName = inName($("#name").html()) || function(){};
+$('#name').html(iName);
+});
 });
 
 /*
@@ -232,12 +232,12 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,251 @@
+// Udacity - Project 2: Resume Builder
+// Kris Hamilton - April 2015
+
+
+// Storing JSON data/objects for Bio into a variable called 'bio'
+var bio = {
+	"name": "Kris Hamilton",
+	"role": "Digital Jedi",
+	"contacts": {
+		"mobile": "+64 27 291 2314",
+		"email": "krisjhamilton@outlook.com",
+		"github": "krisjhamilton",
+		"twitter": "@AKStudioDev",
+		"location": "Wellington, NZ"
+	},
+	"welcomeMessage": "A website developer, musician and Pebbler",
+	"skills": ["Business Analysis", "HTML/CSS", "PHP/MySQL", "Pebble Development", "Digital Jedi"],
+	"bioPic": "images/kris.jpg"
+}
+
+// Function to append Bio from the 'bio' JSON data/objects
+bio.display = function() {
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header")
+		.prepend(formattedRole)
+		.prepend(formattedName);
+
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+	$("#topContacts")
+		.append(formattedMobile)
+        .append(formattedEmail)
+        .append(formattedTwitter)
+        .append(formattedGithub)
+        .append(formattedLocation);
+
+    $("#footerContacts")
+    	.append(formattedMobile)
+        .append(formattedEmail)
+        .append(formattedTwitter)
+        .append(formattedGithub)
+        .append(formattedLocation);
+
+	var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+	var formattedMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedBioPic);
+	$("#header").append(formattedMessage);
+	
+	if(bio.skills.length > 0){	
+		if (bio.skills.length > 0) {
+	        $('#header').append(HTMLskillsStart);
+	        bio.skills.forEach(function (skill) {
+	            var formattedSkill = HTMLskills.replace("%data%", skill);
+	            $('#skills').append(formattedSkill);
+	        });
+	    }	
+	}
+}
+
+// Storing JSON data/objects for Work into a variable called 'work'
+var work = {
+	"jobs": [
+		{
+			"employer": "TEC",
+			"title": "Business Analyst",
+			"location": "Wellington",
+			"dates": "2014 - Present",
+			"description": "Working on the Business Improvement Project to capture, map and improve the current state processes"
+		},
+		{
+			"employer": "BP Oil NZ Ltd",
+			"title": "Business Analyst and Business Improvement Project Lead",
+			"location": "Wellington",
+			"dates": "2012 - 2014",
+			"description": "Gathering business requirements and working with stakeholder and vendors to implement enhancements to the companies core loyalty platform"
+		}
+	]
+}
+
+// Function to append Work from the 'work' JSON data/objects
+work.display = function() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		$(".work-entry:last").append(formattedLocation);
+		
+	
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+
+	}
+
+}
+
+// Storing JSON data/objects for Education into a variable called 'education'
+var education = {
+	"schools": [
+		{
+			"name": "Weltec",
+			"location": "Petone, NZ",
+			"degree": "Certificate in Web Design",
+			"majors": ["PHP/MySQL"],
+			"dates": 2012,
+			"url": "http://weltec.ac.nz"
+		}
+	],
+	"onlineSchool": [
+		{
+			"title": "HTML5 and CSS",
+			"school": "Code Academy",
+			"dates": 2013,
+			"url": "http://codeacademy.com"
+		},
+		{
+			"title": "PHP",
+			"school": "Code Academy",
+			"dates": 2013,
+			"url": "http://codeacademy.com"
+		}
+	]
+}
+
+// Function to append Education from the 'education' JSON data/objects
+education.display = function(){
+    education.schools.forEach(function(school) {
+        $("#education").append(HTMLschoolStart);
+        var formattedSchoolName = HTMLschoolName.replace("%data%", school.name).replace('#', school.url);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+        $(".education-entry:last").append(formattedSchoolName);
+        $(".education-entry:last a").append(formattedSchoolDegree);
+        $(".education-entry:last").append(formattedSchoolLocation).append(formattedSchoolDates);
+
+        school.majors.forEach(function(major) {
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", major);
+            $(".education-entry:last").append(formattedSchoolMajor);
+        });
+    });
+
+    if (education.onlineSchool.length > 0) {
+        $(".education-entry").append(HTMLonlineClasses);
+        education.onlineSchool.forEach(function (course) {
+            var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", course.title).replace('#', course.url);;
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", course.school);
+            var formattedOnlineDates = HTMLonlineDates.replace("%data%", course.dates);
+
+            $(".education-entry:last")
+                .append(formattedOnlineTitle + formattedOnlineSchool)
+                .append(formattedOnlineDates)
+                .append('<br>');
+        });
+    }
+};
+
+// Storing JSON data/objects for Projects into a variable called 'projects'
+var projects = {
+	"projects": [
+		{
+			"title": "Destiny Pebble Tracker",
+			"dates": "06-04-2015",
+			"description": "Destiny tracker for up to date information on your characters, including: <br /> -Power Level, <br /> -Glimmer, <br /> -Grimoire Score, <br /> -Vanguard/Cruicible/Cryptarch levels and progression, <br /> -Primary and Secondary stats.",
+			"images": ["images/pebble.jpg"]
+		},
+		{
+			"title": "Destroy The Night",
+			"dates": "2012 - 2013",
+			"description": "Playing lead guitar in Wellington local band Destroy The Night. The band release a 6 track EP on iTunes, Amazon, Spotify and many more places",
+			"images": ["images/theBand.jpg"]
+		}
+	]
+}
+
+// Function to append Projects from the 'projects' JSON data/objects
+projects.display = function() {
+	for(project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+		
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedTitle);
+		$(".project-entry:last").append(formattedDates);
+		$(".project-entry:last").append(formattedDescription);
+		
+		if (projects.projects[project].images.length > 0) {
+			for (image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+
+// Click tracker - logging the users mouse clicks
+$(document).click(function(loc) { 
+	var x = loc.pageX;
+	var y = loc.pageY; 
+	logClicks(x,y);
+});
+
+
+// Localisation Button
+// 		Note: I had to update helper.js as per stated in the forum link -
+// 		http://forums.udacity.com/questions/100250412/uncaught-typeerror-cannot-read-property-touppercase-of-undefined
+function inName(name) {
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	return name[0] +" "+name[1];
+}
+
+// Calling functions for all sections
+bio.display();
+work.display();
+education.display();
+projects.display();
+
+// Appending the Internationalize button
+$('#main').append(internationalizeButton);
+
+// Appending the Google Map
+$("#mapDiv").append(googleMap);
+
+
+
+
+
+
+
+
+
+
+
+
